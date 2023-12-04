5bee9ce42904f82a4ef3f87f1dbb6774916ca4de
czycha <bruno@goforward.com>
2023-12-03 20:31:59 -0800

    day 1

---
+++ days/1/index.test.ts
@@ -0,0 +1,59 @@
+import { expect, test, describe } from 'bun:test';
+import * as Suite from './lib';
+
+describe('stringToNumber', () => {
+  test('handles number case', () => {
+    expect(Suite.stringToNumber('5')).toEqual(5);
+  });
+
+  test('handles written out case', () => {
+    expect(Suite.stringToNumber('five')).toEqual(5);
+  });
+})
+
+describe('findCalibrationValue', () => {
+  test('finds invalid inputs', () => {
+    expect(() => {
+      Suite.findCalibrationValue('');
+    }).toThrow()
+    expect(() => {
+      Suite.findCalibrationValue('abc');
+    }).toThrow();
+  });
+
+  test('case 1: two values', () => {
+    expect(Suite.findCalibrationValue('t6es9t')).toEqual(69);
+    expect(Suite.findCalibrationValue('56')).toEqual(56);
+  })
+
+  test('case 2: one value', () => {
+    expect(Suite.findCalibrationValue('t6est')).toEqual(66);
+  });
+
+  test('case 3: 3+ values', () => {
+    expect(Suite.findCalibrationValue('t6e7st8')).toEqual(68); 
+    expect(Suite.findCalibrationValue('5456')).toEqual(56); 
+  });
+
+  test('case 4: discrete string values', () => {
+    expect(Suite.findCalibrationValue('tsixes1oneoneonesevenninet')).toEqual(69);
+  });
+
+  test('case 5: overlapping string values', () => {
+    expect(Suite.findCalibrationValue('five61oneightr')).toEqual(58);
+  })
+});
+
+describe('getCalibrationValuesSum', () => {
+  test('skips invalid inputs', () => {
+    expect(Suite.getCalibrationValuesSum(`abc\nt3es6t`)).toEqual(36);;
+  });
+
+  test('calculates correctly', () => {
+    expect(
+      Suite.getCalibrationValuesSum(
+        `t6es9t\nt6est\nt6e7st8\ntsixes1oneoneonesevenninet1`,
+      ),
+    ).toEqual(69 + 66 + 68 +61);
+  });
+});
---
+++ days/1/lib.ts
@@ -0,0 +1,69 @@
+import picocolors from 'picocolors';
+
+const NUMERALS: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+const NUMERAL_KEYS = Object.keys(NUMERALS)
+const REGEX = new RegExp(`(?=([1-9]|${Object.keys(NUMERALS).join('|')}))`, 'gi');
+
+export function stringToNumber(str: string): number {
+  if (str in NUMERALS) return NUMERALS[str];
+
+  return parseInt(str);
+}
+
+export function findCalibrationValue(input: string): number {
+  const matches = [...input.matchAll(REGEX)].map(m => m[1]);
+
+  if (matches.length ===0) {
+    throw new Error('Invalid calibration value');
+  }
+
+  return stringToNumber(matches[0]) * 10 + stringToNumber(matches.at(-1)!);
+}
+
+function readFile(path: string): Promise<string> {
+  const file = Bun.file(path);
+  return file.text();
+}
+
+export function getCalibrationValuesSum(text: string): number {
+  let sum = 0;
+  const lines = text.split('\n');
+  for (const line of lines) {
+    try {
+      const value = findCalibrationValue(line.toLowerCase());
+      sum += value;
+    } catch (e) {
+      console.warn(
+        picocolors.yellow(`Could not read calibration value: "${line}"`),
+      );
+    }
+  }
+
+  return sum;
+}
+
+export async function readCalibrationValues(path: string): Promise<number> {
+  const text = await readFile(path);
+  return getCalibrationValuesSum(text);
+}
+
+export async function main() {
+  const path = process.argv[2];
+  if (!path) {
+    throw new Error(
+      'Please provide a path to read your calibration values from',
+    );
+  }
+  const sum = await readCalibrationValues(path);
+  console.log(`Result: ${picocolors.green(sum)}`);
+}
